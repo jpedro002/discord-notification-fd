1fb2323545360e8e6caef8ded73c5c6707a696e9
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-07 12:05:02 -0300

    exclude log

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -2,7 +2,6 @@ const core = require("@actions/core");
 
 async function run() {
   const eventName = process.env.GITHUB_EVENT_NAME
-  console.log(eventName);
   core.setOutput("event-output", eventName);
 }
 
