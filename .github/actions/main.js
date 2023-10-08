75ef6632638184bd53f4e53268d1dbeae469ae1f
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-08 14:31:35 -0300

    fix

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -3,6 +3,8 @@ const core = require("@actions/core");
 async function run() {
   const eventName = process.env.GITHUB_EVENT_NAME
   core.setOutput("event-output", eventName);
+  console.log(eventName);
+
 }
 
 run();
