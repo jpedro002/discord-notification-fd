e364e0b46e92848af892b4990f46fe8199f8045b
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-09 18:54:37 -0300

    fix

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -11,6 +11,16 @@ console.log(
 		context.payload.pull_request.merged
 );
 
+const identifyMessage = "Merge pull request";
+const mensagemDoCommitMaisRecente = context.payload.commits[context.payload.commits.length - 1].message;
+
+if (mensagemDoCommitMaisRecente.toLowerCase().includes(identifyMessage.toLowerCase())) {
+  console.log("é igual uhuulll");
+  console.log(mensagemDoCommitMaisRecente);
+} else {
+  console.log("não é igual");
+}
+
 sendDiscordMessage(DISCORD_WEBHOOK, DISCORD_PERSONALIZED_EMBED).catch(
 	(error) => {
 		console.error("Error sending Discord message:", error);
