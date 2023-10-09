b6d017bdc12e5fdd9ae34dc496b1225ebb20fc44
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-09 18:13:34 -0300

    fix

--- .github/actions/utils/fillEmbed.js
+++ .github/actions/utils/fillEmbed.js
@@ -1,5 +1,5 @@
-const { getAuthorAvatar  } = require("./api.js");
-const {DEFAULT_EMBED,DEFAULT_MESSAGES} = require("./data-embeds.js");
+const { getAuthorAvatar } = require("./api.js");
+const { DEFAULT_EMBED, DEFAULT_MESSAGES } = require("./data-embeds.js");
 const github = require("@actions/github");
 const context = github.context;
 
@@ -11,8 +11,6 @@ const {
 	MENSAGE_ON_PULL_REQUEST_MERGED,
 	MENSAGE_ON_ISSUE_OPENED,
 } = process.env;
-
-
 
 const fillDefaultEmbed = async () => {
 	const githubActor = GITHUB_ACTOR;
@@ -29,32 +27,31 @@ const fillDefaultEmbed = async () => {
 			if (context.payload.action === "opened") {
 				DEFAULT_EMBED.embeds[0].description =
 					MENSAGE_ON_PULL_REQUEST_OPENED || DEFAULT_MESSAGES.pr_opened;
-
-
-			} else if (
-				context.payload.action === "closed" &&
-				context.payload.pull_request &&
-				context.payload.pull_request.merged
-			) {
-				console.log(context.payload.action === "closed" &&
-				context.payload.pull_request &&
-				context.payload.pull_request.merged);
-
-				DEFAULT_EMBED.embeds[0].description =
-					MENSAGE_ON_PULL_REQUEST_MERGED || DEFAULT_MESSAGES.pr_acepted;
+			} else {
+				process.exit(1);
+				console.log('Pull Request event not supported');
 			}
 			break;
 		case "issues":
 			DEFAULT_EMBED.embeds[0].description =
 				MENSAGE_ON_ISSUE_OPENED || DEFAULT_MESSAGES.issue;
-				DEFAULT_EMBED.embeds[0].footer.text = `Conteudo da issue ${context.payload.issue.body}` 
+			DEFAULT_EMBED.embeds[0].footer.text = `Conteudo da issue ${context.payload.issue.body}`;
 			break;
 		case "push":
+			const mensagemDoCommitMaisRecente =
+				context.payload.commits[context.payload.commits.length - 1].message;
+
+			if (mensagemDoCommitMaisRecente.includes("Merge pull request")) {
+				DEFAULT_EMBED.embeds[0].description =
+					MENSAGE_ON_PULL_REQUEST_MERGED || DEFAULT_MESSAGES.pr_acepted;
+			} else {
+			}
 			DEFAULT_EMBED.embeds[0].description =
 				MENSAGE_ON_PUSH || DEFAULT_MESSAGES.push;
 			DEFAULT_EMBED.embeds[0].footer.text = `O commit que disparou a mensagem: ${
 				context.payload.commits[context.payload.commits.length - 1].message
 			}`;
+
 			break;
 		default:
 			console.error("Event not supported");
@@ -63,4 +60,4 @@ const fillDefaultEmbed = async () => {
 	return DEFAULT_EMBED;
 };
 
-module.exports = fillDefaultEmbed
+module.exports = fillDefaultEmbed;
