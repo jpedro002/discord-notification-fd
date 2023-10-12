ee701d472719f690327cfa7c0784e32ad7856190
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-12 20:27:13 -0300

    fix

--- .github/actions/utils/sendDiscordMessage.js
+++ .github/actions/utils/sendDiscordMessage.js
@@ -13,8 +13,7 @@ const sendDiscordMessage = async (DISCORD_WEBHOOK,DISCORD_PERSONALIZED_EMBED) =>
 		DISCORD_PERSONALIZED_EMBED
 	) {
 		const embed = JSON.parse(DISCORD_PERSONALIZED_EMBED)
-		console.log('log aqui',JSON.stringify(embed[0].embeds, null, 2));
-		postMessage(DISCORD_WEBHOOK, JSON.stringify(DISCORD_PERSONALIZED_EMBED[0]));
+		postMessage(DISCORD_WEBHOOK, JSON.stringify(embed[0], null, 2));
 	} else {
 		try {
 			const data = await fillDefaultEmbed();
