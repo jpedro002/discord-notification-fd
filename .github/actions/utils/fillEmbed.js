8b5ee15f05e7f349fe4380cabc81aa929d063c50
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-12 13:34:09 -0300

    feat logo on action

--- .github/actions/utils/fillEmbed.js
+++ .github/actions/utils/fillEmbed.js
@@ -6,80 +6,76 @@ const core = require("@actions/core");
 const context = github.context;
 
 const {
-  GITHUB_REPOSITORY,
-  GITHUB_ACTOR,
-  MENSAGE_ON_PUSH,
-  MENSAGE_ON_PULL_REQUEST_OPENED,
-  MENSAGE_ON_PULL_REQUEST_MERGED,
-  MENSAGE_ON_ISSUE_OPENED,
+	GITHUB_REPOSITORY,
+	GITHUB_ACTOR,
+	MENSAGE_ON_PUSH,
+	MENSAGE_ON_PULL_REQUEST_OPENED,
+	MENSAGE_ON_PULL_REQUEST_MERGED,
+	MENSAGE_ON_ISSUE_OPENED,
 } = process.env;
 
 const fillDefaultEmbed = async () => {
-  const githubActor = GITHUB_ACTOR;
-  const avatarUrl = await getAuthorAvatar(githubActor);
-  const color = Math.floor(Math.random() * 16777215) + 1;
+	const githubActor = GITHUB_ACTOR;
+	const avatarUrl = await getAuthorAvatar(githubActor);
+	const color = Math.floor(Math.random() * 16777215) + 1;
 
-  const embed = {
-    ...DEFAULT_EMBED,
-    embeds: [
-      {
-        ...DEFAULT_EMBED.embeds[0],
-        color,
-        author: {
-          name: githubActor,
-          icon_url: avatarUrl,
-        },
-        title: "Repositório",
-        url: `https://github.com/${GITHUB_REPOSITORY}`,
-      },
-    ],
-  };
+	const embed = {
+		embeds: [
+			{
+				...DEFAULT_EMBED.embeds[0],
+				color,
+				author: {
+					name: githubActor,
+					icon_url: avatarUrl,
+				},
+				title: "Repositório",
+				url: `https://github.com/${GITHUB_REPOSITORY}`,
+			},
+		],
+	};
 
-  switch (context.eventName) {
-    case "pull_request":
-      if (context.payload.action === "opened") {
-        embed.embeds[0].description =
-          MENSAGE_ON_PULL_REQUEST_OPENED || DEFAULT_MESSAGES.pr_opened;
-					console.log(JSON.stringify(embed,null,2))
-      }else {
-        console.log("Pull Request event not supported");
-        process.exit(1);
-      }
-      break;
-    case "issues":
-      embed.embeds[0].description =
-        MENSAGE_ON_ISSUE_OPENED || DEFAULT_MESSAGES.issue;
-      embed.embeds[0].footer.text = `Conteúdo da issue: ${context.payload.issue.body}`;
-			console.log(JSON.stringify(embed,null,2))
-      break;
-    case "push":
-      const mensagemDoCommitMaisRecente =
-        context.payload.commits[context.payload.commits.length - 1].message;
-      const identifyMessage = "Merge pull request";
-
-      if (mensagemDoCommitMaisRecente.toLowerCase().includes(identifyMessage.toLowerCase())) {
-				console.log("é igual uhuulll");
-        embed.embeds[0].description =
-				MENSAGE_ON_PULL_REQUEST_MERGED || DEFAULT_MESSAGES.pr_acepted;
-				console.log(JSON.stringify(embed,null,2))
-				
-      } else {
-				console.log("não é igual");
-        embed.embeds[0].description =
-				MENSAGE_ON_PUSH || DEFAULT_MESSAGES.push;
-        embed.embeds[0].footer.text = `O commit que disparou a mensagem: ${
-					context.payload.commits[context.payload.commits.length - 1].message
-        }`;
-				console.log(JSON.stringify(embed,null,2))
-      }
-      break;
-			default:
-				console.error("Event not supported");
+	switch (context.eventName) {
+		case "pull_request":
+			if (context.payload.action === "opened") {
+				embed.embeds[0].description =
+					MENSAGE_ON_PULL_REQUEST_OPENED || DEFAULT_MESSAGES.pr_opened;
+			} else {
+				console.log("Pull Request event not supported");
 				process.exit(1);
 			}
-			
-			return embed;
-		};
-		
-		module.exports = fillDefaultEmbed;
-		
+			break;
+		case "issues":
+			embed.embeds[0].description =
+				MENSAGE_ON_ISSUE_OPENED || DEFAULT_MESSAGES.issue;
+			embed.embeds[0].footer.text = `Conteúdo da issue: ${context.payload.issue.body}`
+			break;
+		case "push":
+			const mensagemDoCommitMaisRecente =
+				context.payload.commits[context.payload.commits.length - 1].message;
+			const identifyMessage = "Merge pull request";
+
+			if (
+				mensagemDoCommitMaisRecente
+					.toLowerCase()
+					.includes(identifyMessage.toLowerCase())
+			) {
+				
+				embed.embeds[0].description =
+					MENSAGE_ON_PULL_REQUEST_MERGED || DEFAULT_MESSAGES.pr_acepted;
+			} else {
+				console.log("não é igual");
+				embed.embeds[0].description = MENSAGE_ON_PUSH || DEFAULT_MESSAGES.push;
+				embed.embeds[0].footer.text = `O commit que disparou a mensagem: ${
+					context.payload.commits[context.payload.commits.length - 1].message
+				}`;
+			}
+			break;
+		default:
+			console.error("Event not supported");
+			process.exit(1);
+	}
+
+	return embed;
+};
+
+module.exports = fillDefaultEmbed;
