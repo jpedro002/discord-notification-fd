910b787f1f3b34e34cfc066a7df2ce8c43424892
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-12 20:42:53 -0300

    fix

--- .github/actions/utils/sendDiscordMessage.js
+++ .github/actions/utils/sendDiscordMessage.js
@@ -13,7 +13,7 @@ const sendDiscordMessage = async (DISCORD_WEBHOOK,DISCORD_PERSONALIZED_EMBED) =>
 		DISCORD_PERSONALIZED_EMBED
 	) {
 		const parseEmbed = JSON.parse(DISCORD_PERSONALIZED_EMBED)
-		const embedToSend = JSON.stringify(parseEmbed[0])
+		const embedToSend = parseEmbed[0]
 		console.log(embedToSend,"embedToSend");
 		postMessage(DISCORD_WEBHOOK, embedToSend);
 	} else {
