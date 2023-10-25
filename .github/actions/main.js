5ceb592385b8e57fd5dfb13c97c181dd554205b5
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-25 14:46:46 -0300

    fix teste

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -2,8 +2,6 @@ const sendDiscordMessage = require("./utils/sendDiscordMessage");
 
 const { DISCORD_WEBHOOK, DISCORD_PERSONALIZED_EMBED } = process.env;
 
-console.log(DISCORD_PERSONALIZED_EMBED);
-
 sendDiscordMessage(DISCORD_WEBHOOK, DISCORD_PERSONALIZED_EMBED).catch(
 	(error) => {
 		console.error("Error sending Discord message:", error);
--- .github/actions/utils/sendDiscordMessage.js
+++ .github/actions/utils/sendDiscordMessage.js
@@ -5,7 +5,9 @@ const fillDefaultEmbed = require("./fillEmbed.js")
 
 
 const sendDiscordMessage = async (DISCORD_WEBHOOK,DISCORD_PERSONALIZED_EMBED) => {
+
 	
+
 	if (!DISCORD_WEBHOOK) {
 		console.error("DISCORD_WEBHOOK is not defined");
 		process.exit(1);
@@ -14,14 +16,14 @@ const sendDiscordMessage = async (DISCORD_WEBHOOK,DISCORD_PERSONALIZED_EMBED) =>
 	) {
 		const parseEmbed = JSON.parse(DISCORD_PERSONALIZED_EMBED)
 		const embedToSend = parseEmbed[0]
-		console.log(embedToSend,"embedToSend");
+		console.log(embedToSend,"embed To Send");
 		postMessage(DISCORD_WEBHOOK, embedToSend);
 	} else {
 		try {
 			const data = await fillDefaultEmbed();
 			console.log(JSON.stringify(data, null, 2));
 			console.log(DISCORD_WEBHOOK);
-			await postMessage(DISCORD_WEBHOOK,data);
+			await postMessage(DISCORD_WEBHOOK,JSON.stringify(data));
 		} catch (error) {
 			console.error("Error:", error);
 		}
